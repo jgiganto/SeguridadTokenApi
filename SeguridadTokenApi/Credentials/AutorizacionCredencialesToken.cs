bfd41d6931b2434c242120a9eff8a26979b9673f
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-04-18 21:13:52 +0200

    a

---
+++ SeguridadTokenApi/Credentials/AutorizacionCredencialesToken.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using SeguridadTokenApi.Models;
+
+namespace SeguridadTokenApi.Credentials
+{
+    public class AutorizacionCredencialesToken:OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+            
+            using (EntidadEmpleados db = new EntidadEmpleados())
+            {
+                
+                int empno = int.Parse(context.Password);
+                var empleado = db.EMP.FirstOrDefault(z => z.APELLIDO == context.UserName && z.EMP_NO == empno);
+                 
+                if (empleado == null)
+                {
+                    context.SetError("Acceso denegado", "El usuario/password son incorrectos.");
+                    return;
+                }
+                ClaimsIdentity identidad = new ClaimsIdentity(context.Options.AuthenticationType);
+                identidad.AddClaim(new Claim(ClaimTypes.Name, context.Password));
+                identidad.AddClaim(new Claim(ClaimTypes.Role, "EMPLEADO"));
+                context.Validated(identidad);
+                 
+            }
+
+        }
+    }
+}
---
+++ SeguridadTokenApi/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(SeguridadTokenApi.Startup))]
+
+namespace SeguridadTokenApi
+{
+    public class Startup
+    {
+        public void ConfigurarOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions opcionesautorizacion
+                = new OAuthAuthorizationServerOptions()
+                {
+                    //PERMITIMOS AUTORIZACIONES HTTP
+                    AllowInsecureHttp = true,
+                    //INCLUIMOS UNA RUTA PARA ACCEDER A LA GENERACION
+                    //DEL TOKEN
+                    TokenEndpointPath = new PathString("/recuperartoken"),
+                    //TIEMPO QUE EL TOKEN NOS PERMITIRA ACCEDER A LAS
+                    //PETICIONES GET SIN NECESIDAD DE GENERAR NINGUN OTRO
+                    AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(10),
+                    //CLASE ENCARGADA DE VALIDAR SI EL USUARIO
+                    //TIENE ACCESO A LA GENERACION DE TOKENS
+                    Provider = new Credentials.AutorizacionCredencialesToken(),
+                };
+            //AÑADIMOS LA AUTORIZACION A NUESTRA APLICACION
+            app.UseOAuthAuthorizationServer(opcionesautorizacion);
+            //AHORA INDICAMOS QUE LA AUTORIZACION ESTARA
+            //ACTIVA MEDIANTE BEARER (OAUTH 2.0)
+            OAuthBearerAuthenticationOptions opcionesoauth =
+                new OAuthBearerAuthenticationOptions()
+                {
+                    AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode.Active
+                };
+            app.UseOAuthBearerAuthentication(opcionesoauth);
+        }
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            //REGISTRAMOS LA CLASE DE CONFIGURACION
+            //DE APIS
+            WebApiConfig.Register(config);
+            //INCLUIMOS EL METODO QUE SE 
+            //ENCARGARA DE LAS PETICIONES MIDDLEWARE
+            ConfigurarOAuth(app);
+            //ANTES DE LAS PETICIONES, EL METODO
+            //HABRA AUTORIZADO LOS TOKENS
+            //POR LO QUE INDICAMOS DESPUES QUE
+            //CONFIGURACION UTILIZARA NUESTRO API
+            app.UseWebApi(config);
+        }
+
+
+    }
+}
